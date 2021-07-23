effb9723b6839c2d79a8b50eea43248c8a74c0b2
Juan Manuel Rivera <jm.rivera@uniandes.edu.co>
2021-07-23 03:16:41 -0500

    Añade método para reproducir mp3

--- ControlSobresalto.py
+++ ControlSobresalto.py
@@ -7,7 +7,9 @@ Este es un archivo temporal.
 
 import serial
 import time
+import pygame
 
+pygame.mixer.init()
 
 arduino=serial.Serial('COM6',57600)
 time.sleep(2)
@@ -18,11 +20,20 @@ while True:
     a=arduinoOutput.decode('UTF-8')
     #print(a)
     if 'DONETodo listo' in a:
+ #       pygame.mixer.music.load("Audios/02 - habituación a la caja - 5 minutos.mp3")
+ #       pygame.mixer.music.play()
+ #       time.sleep(300)
+ #       pygame.mixer.music.load("Audios/05 -  sobresalto e inhibicion- 30 minutos.mp3")
+ #       pygame.mixer.music.play()
         arduino.write(bytes('p', 'utf-8'))
-        while 'Datos tomados.' not in a:
-            arduinoOutput=arduino.readline()
-            a=arduinoOutput.decode('UTF-8')
-            f.write(a)
-            print(a)
+        b=arduino.readline().decode('UTF-8')
+        print(b)
+        while 'Datos tomados' not in b:
+            #arduinoOutput=arduino.readline()
+            b=arduino.readline().decode('UTF-8')
+            f.write(b)
+            print(b)
     break
 f.close()
+
+pygame.mixer.Sound("Audios/02 - habituación a la caja - 5 minutos.mp3")
